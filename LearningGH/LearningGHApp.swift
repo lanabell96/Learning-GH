1a719091cfe1b819ec1d2ef0d3e14162cba8ac0d
Alana Richmond <alanarichmond4@gmail.com>
2021-07-11 15:38:55 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Alana Richmond on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
